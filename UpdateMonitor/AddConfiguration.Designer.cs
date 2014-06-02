3213341b0ada98ab030ad9d9e0dc156f4c438a28
Radu Danciu <rhadoodc@yahoo.com>
2014-06-03 00:32:11 +0300

    Better form consistency

--- UpdateMonitor/AddConfiguration.Designer.cs
+++ UpdateMonitor/AddConfiguration.Designer.cs
@@ -109,7 +109,7 @@
 			this.Controls.Add(this.cancelButton);
 			this.Controls.Add(this.createButton);
 			this.Controls.Add(this.configNameTextBox);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
